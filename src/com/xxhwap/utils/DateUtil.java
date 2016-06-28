033db404847d5bdf81f2628ad464c082cb9dcf9b
zhengyunfei <zhengyf@ctyun.cn>
2016-06-28 09:43:32 +0800

    部分表格间距不一致，应客主要求调整一直

--- src/com/xxhwap/utils/DateUtil.java
+++ src/com/xxhwap/utils/DateUtil.java
@@ -24,10 +24,8 @@ public class DateUtil {
             Date dt1 = df.parse(DATE1);
             Date dt2 = df.parse(DATE2);
             if (dt1.getTime() > dt2.getTime()) {
-                System.out.println("dt1 在dt2前");
                 return 1;
             } else if (dt1.getTime() < dt2.getTime()) {
-                System.out.println("dt1在dt2后");
                 return -1;
             } else {
                 return 0;
